ccb92c3f788bda2fc086e59a3613fd68c69d7be1
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-06-09 10:14:04 +1000

    [QOL-8052] synchronise CI config with Open Data theme and add Github Actions workflow

--- .docker/scripts/init.sh
+++ .docker/scripts/init.sh
@@ -8,18 +8,16 @@ CKAN_USER_NAME="${CKAN_USER_NAME:-admin}"
 CKAN_DISPLAY_NAME="${CKAN_DISPLAY_NAME:-Administrator}"
 CKAN_USER_PASSWORD="${CKAN_USER_PASSWORD:-Password123!}"
 CKAN_USER_EMAIL="${CKAN_USER_EMAIL:-admin@localhost}"
-CKAN_INI_FILE=/app/ckan/default/production.ini
 
-. /app/ckan/default/bin/activate
+. ${APP_DIR}/bin/activate
 
 ckan_cli () {
     if (which ckan > /dev/null); then
-        ckan -c ${CKAN_INI_FILE} "$@"
+        ckan -c ${CKAN_INI} "$@"
     else
-        paster --plugin=ckan "$@" -c ${CKAN_INI_FILE}
+        paster --plugin=ckan "$@" -c ${CKAN_INI}
     fi
 }
-cd /app/ckan/default/src/ckan
 ckan_cli db clean || exit 1
 ckan_cli db init || exit 1
 ckan_cli user add "${CKAN_USER_NAME}"\
@@ -29,4 +27,4 @@ ckan_cli user add "${CKAN_USER_NAME}"\
 ckan_cli sysadmin add "${CKAN_USER_NAME}" || exit 1
 
 # Create some base test data
-. /app/scripts/create-test-data.sh
+. $WORKDIR/scripts/create-test-data.sh
