69b55b4ee699644d9ef4421e25555b84121107bd
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-01-21 11:48:35 +1000

    [QOL-8594] sync test config with ckanext-datarequests
    - some groundwork for CKAN 2.9
    - remove unneeded config

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -4,9 +4,19 @@ from behaving.web.steps import *  # noqa: F401, F403
 from behaving.web.steps.url import when_i_visit_url
 
 
+@step(u'I get the current URL')
+def get_current_url(context):
+    context.browser.evaluate_script("document.documentElement.clientWidth")
+
+
 @step('I go to homepage')
 def go_to_home(context):
     when_i_visit_url(context, '/')
+
+
+@step('I go to register page')
+def go_to_register_page(context):
+    when_i_visit_url(context, '/user/register')
 
 
 @step('I log in')
@@ -15,10 +25,24 @@ def log_in(context):
     context.execute_steps(u"""
         When I go to homepage
         And I click the link with text that contains "Log in"
-        And I fill in "login" with "$name"
+        And I log in directly
+    """)
+
+
+@step('I log in directly')
+def log_in_directly(context):
+    """
+    This differs to the `log_in` function above by logging in directly to a page where the user login form is presented
+    :param context:
+    :return:
+    """
+
+    assert context.persona
+    context.execute_steps(u"""
+        When I fill in "login" with "$name"
         And I fill in "password" with "$password"
         And I press the element with xpath "//button[contains(string(), 'Login')]"
-        Then I should see an element with xpath "//a[contains(string(), 'Log out')]"
+        Then I should see an element with xpath "//a[@title='Log out']"
     """)
 
 
@@ -27,11 +51,30 @@ def go_to_dataset_page(context):
     when_i_visit_url(context, '/dataset')
 
 
+@step(u'I go to dataset "{name}"')
+def go_to_dataset(context, name):
+    when_i_visit_url(context, '/dataset/' + name)
+
+
 @step('I go to organisation page')
 def go_to_organisation_page(context):
     when_i_visit_url(context, '/organization')
 
 
-@step('I go to register page')
-def go_to_register_page(context):
-    when_i_visit_url(context, '/user/register')
+@step(u'I set persona var "{key}" to "{value}"')
+def set_persona_var(context, key, value):
+    context.persona[key] = value
+
+
+@step('I log in and go to admin config page')
+def log_in_go_to_admin_config(context):
+    assert context.persona
+    context.execute_steps(u"""
+        When I log in
+        And I go to admin config page
+    """)
+
+
+@step('I go to admin config page')
+def go_to_admin_config(context):
+    when_i_visit_url(context, '/ckan-admin/config')
