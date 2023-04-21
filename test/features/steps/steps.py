1221a1ee0e3b11bb4a4ad5ad32df490faafd27c9
antuarc <carl.antuar@smartservice.qld.gov.au>
2023-04-21 11:14:57 +1000

    [QOLDEV-368] tighten Flake8 rules

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -160,12 +160,16 @@ def go_to_user_show(context, user_id):
 
 @step(u'I view the "{group_id}" group API "{including}" users')
 def go_to_group_including_users(context, group_id, including):
-    when_i_visit_url(context, r'/api/3/action/group_show?id={}&include_users={}'.format(group_id, including in ['with', 'including']))
+    when_i_visit_url(
+        context, r'/api/3/action/group_show?id={}&include_users={}'.format(
+            group_id, including in ['with', 'including']))
 
 
 @step(u'I view the "{organisation_id}" organisation API "{including}" users')
 def go_to_organisation_including_users(context, organisation_id, including):
-    when_i_visit_url(context, r'/api/3/action/organization_show?id={}&include_users={}'.format(organisation_id, including in ['with', 'including']))
+    when_i_visit_url(
+        context, r'/api/3/action/organization_show?id={}&include_users={}'.format(
+            organisation_id, including in ['with', 'including']))
 
 
 @step(u'I should be able to download via the element with xpath "{expression}"')
